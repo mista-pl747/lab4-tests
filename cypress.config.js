6005633273fedfe686abf3d25a882d6304babf61
Daniil <daniilhornostaiev646@gmail.com>
2025-11-11 18:33:26 +0100

    Ci Run

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "23vnc7",
     baseUrl: "http://127.0.0.1:8000",    
 
     specPattern: "cypress/e2e/**/*.cy.js",
