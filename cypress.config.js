6a5291f3e55b7d966393ae247d1cd32e550e7246
Daniil <daniilhornostaiev646@gmail.com>
2025-11-11 18:26:42 +0100

    Ci Run

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://127.0.0.1:8000",    
+
+    specPattern: "cypress/e2e/**/*.cy.js",
+
+    supportFile: false,
+    video: false,
+    screenshotOnRunFailure: false,
+    
+    setupNodeEvents(on, config) {
+    },
+  },
+});
